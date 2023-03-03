3383f9a1044fcf5f50eed53816d3dbaa6981a071
Diljot Grewal <diljot.grewal@gmail.com>
2023-03-03 13:26:49 -0800

    code updates

--- scrna_demultiplexing_utils/utils.py
+++ scrna_demultiplexing_utils/utils.py
@@ -2,15 +2,10 @@ import errno
 import os
 from subprocess import Popen, PIPE
 
+import pandas as pd
+import yaml
 
-FEATURE_TYPE_MAP = {
-    'gene_expression': 'Gene Expression',
-    'hto':             'Multiplexing Capture',
-    'cite':            'Multiplexing Capture',
-    'tcr':             'VDJ-T',    
-}
 
-        
 def makedirs(directory, isfile=False):
     if isfile:
         directory = os.path.dirname(directory)
@@ -48,24 +43,24 @@ def run_cmd(cmd, output=None):
         stdout.close()
 
 
-def generate_config(cmo_csv, gex_fastq, multiplex_capture_fastq, reference, config_file):
-
-    with open(config_file, 'wt') as writer:
-
-        writer.write('[gene-expression]\n')
-        writer.write(f'reference,{reference}\n')
-        writer.write(f'cmo-set,{cmo_csv}\n')
+def get_feature_type_map():
+    return {
+        'gene_expression': 'Gene Expression',
+        'hto': 'Multiplexing Capture',
+        'cite': 'Multiplexing Capture',
+        'tcr': 'VDJ-T',
+    }
 
 
 def create_cmo(metadata, cmo_path):
     data = []
     for cmo in metadata['meta']['hashtag']:
         data.append({
-            'id':           cmo,
-            'name':         cmo,
-            'read':         'R2',
-            'pattern':      '^NNNNNNNNNN(BC)NNNNNNNNN',
-            'sequence':     metadata['meta']['hashtag'][cmo]['sequence'],
+            'id': cmo,
+            'name': cmo,
+            'read': 'R2',
+            'pattern': '^NNNNNNNNNN(BC)NNNNNNNNN',
+            'sequence': metadata['meta']['hashtag'][cmo]['sequence'],
             'feature_type': 'Multiplexing Capture',
         })
 
@@ -76,30 +71,33 @@ def create_antibodies(metadata, antibodies_path):
     data = []
     for cmo in metadata['meta']['citeseq']:
         data.append({
-            'id':           cmo,
-            'name':         metadata['meta']['citeseq'][cmo]['protein'].split(' ')[1].replace(',', '').strip(),
-            'read':         'R2',
-            'pattern':      '^NNNNNNNNNN(BC)NNNNNNNNN',
-            'sequence':     metadata['meta']['citeseq'][cmo]['sequence'],
+            'id': cmo,
+            'name': metadata['meta']['citeseq'][cmo]['protein'].split(' ')[1].replace(',', '').strip(),
+            'read': 'R2',
+            'pattern': '^NNNNNNNNNN(BC)NNNNNNNNN',
+            'sequence': metadata['meta']['citeseq'][cmo]['sequence'],
             'feature_type': 'Antibody Capture',
         })
-    
+
     pd.DataFrame(data).to_csv(antibodies_path, index=False)
 
 
 def create_initial_run_multiconfig(
-    metadata_path, 
-    multiconfig_path, 
-    cmo_path, 
-    antibodies_path
+        metadata_path,
+        reference,
+        cmo_path,
+        antibodies_path,
+        multiconfig_path
 ):
+    feature_type_map = get_feature_type_map()
+
     metadata = yaml.load(open(metadata_path))
 
     libraries = {}
     for file in metadata['files']:
         fastq_path = os.path.join(os.path.dirname(metadata_path), os.path.dirname(file))
         fastq_id = metadata['files'][file]['identifier']
-        feature_type = FEATURE_TYPE_MAP[metadata['files'][file]['type']]
+        feature_type = feature_type_map[metadata['files'][file]['type']]
 
         if fastq_id not in libraries.keys():
             libraries[fastq_id] = {
@@ -109,10 +107,10 @@ def create_initial_run_multiconfig(
 
     lines = [
         f'[gene-expression]',
-        f'reference,{settings.referencepath.gex}',
-        f'cmo-set,{cmo}',
+        f'reference,{reference}',
+        f'cmo-set,{cmo_path}',
         f'[feature]',
-        f'reference,{antibodies}',
+        f'reference,{antibodies_path}',
         f'[libraries]',
         f'fastq_id,fastqs,feature_types',
     ]
@@ -122,16 +120,12 @@ def create_initial_run_multiconfig(
             lines.append(f"{fastq_id},{libraries[fastq_id]['fastq_path']},{libraries[fastq_id]['feature_type']}")
 
     lines.append('[samples]'),
-        lines.append('sample_id,cmo_ids')
-        for hashtag in metadata['meta']['hashtag'].keys():
-            lines.append(f"{metadata['meta']['hashtag'][hashtag]['sample_id']},{hashtag}")
-
-    create_cmo(metadata, cmo_path)
-    create_antibodies(metadata, antibodies_path)
+    lines.append('sample_id,cmo_ids')
+    for hashtag in metadata['meta']['hashtag'].keys():
+        lines.append(f"{metadata['meta']['hashtag'][hashtag]['sample_id']},{hashtag}")
 
     with open(multiconfig_path, 'w') as f:
         f.writelines('\n'.join(lines))
-
 
 
 def cellranger_multi(
@@ -141,6 +135,16 @@ def cellranger_multi(
         cite_fastq,
         tempdir
 ):
+    os.makedirs(tempdir)
+
+    cmo_path = os.path.join(tempdir, 'cmo.txt')
+    antibodies_path = os.path.join(tempdir, 'antibodies.txt')
+    multiconfig_path = os.path.join(tempdir, 'multiconfig.txt')
+
+    create_cmo(meta_yaml, cmo_path)
+    create_antibodies(meta_yaml, antibodies_path)
+
+    create_initial_run_multiconfig(meta_yaml, reference, cmo_path, antibodies_path, multiconfig_path)
 
     ...
 
