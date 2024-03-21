93f1a72f9883daae875655189094dc74491959a5
Diljot <diljot.grewal@gmail.com>
2024-03-21 16:49:54 -0400

    small minor updates

--- tests/GEX_CITE_HTO_NO_TCR_BCR/run.sh
+++ tests/GEX_CITE_HTO_NO_TCR_BCR/run.sh
@@ -1,6 +1,6 @@
 module load java/jdk-11.0.11
 module load singularity/3.7.1
 
-../nextflow pull shahcompbio/scrna_demultiplexing -r newrefactor
+../nextflow pull shahcompbio/scrna_demultiplexing -r master
 
-../nextflow run shahcompbio/scrna_demultiplexing -r newrefactor -resume -with-singularity $PWD/../cellranger.sif  -profile lsf,singularity --output_dir outputs --jobmode local --numcores 16 --meta_yaml $PWD/meta.yaml  --gex_fastq $PWD/../data/gex  --gex_id PBMC-ALL_60k_universal_HashAB1-4_BL_4tags_Rep1_gex --cite_fastq $PWD/../data/cite --cite_id PBMC-ALL_60k_universal_HashAB1-4_BL_4tags_Rep1_ab --reference /juno/work/shah/reference/transcriptomes/GRCh38 --vdj_reference /juno/work/shah/reference/transcriptomes/refdata-cellranger-vdj-GRCh38-alts-ensembl-7.1.0 --sample_id SA123 --bcr_fastq $PWD/../data/cite --bcr_id PBMC-ALL_60k_universal_HashAB1-4_BL_4tags_Rep1_ab
+../nextflow run shahcompbio/scrna_demultiplexing -r master -resume -with-singularity $PWD/../cellranger.sif  -profile singularity --output_dir outputs --jobmode local --numcores 16 --meta_yaml $PWD/meta.yaml  --gex_fastq $PWD/../10x_test_data/gex  --gex_id PBMC-ALL_60k_universal_HashAB1-4_BL_4tags_Rep1_gex --cite_fastq $PWD/../10x_test_data/cite --cite_id PBMC-ALL_60k_universal_HashAB1-4_BL_4tags_Rep1_ab --reference $PWD/../reference_grch38_scrna/GRCh38/ --vdj_reference $PWD/../reference_grch38_scrna/refdata-cellranger-vdj-GRCh38-alts-ensembl-7.1.0 --sample_id SA123 --bcr_fastq $PWD/../10x_test_data/cite --bcr_id PBMC-ALL_60k_universal_HashAB1-4_BL_4tags_Rep1_ab
--- tests/get_data.sh
+++ tests/get_data.sh
@@ -1,2 +1,6 @@
 wget https://mondriantestdata.s3.amazonaws.com/10x_test_data.tar.gz
 tar -xvf 10x_test_data.tar.gz
+
+
+wget https://mondriantestdata.s3.amazonaws.com/reference_grch38_scrna.tar.gz
+tar -xvf reference_grch38_scrna.tar.gz
